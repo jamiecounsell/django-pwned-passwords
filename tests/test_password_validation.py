f908d1a7098b0c05dec07a48920711545cb0bb4f
Jamie Counsell <jamiecounsell@me.com>
2017-08-05 16:15:25 -0400

    add tests for help text

--- tests/test_password_validation.py
+++ tests/test_password_validation.py
@@ -60,7 +60,6 @@ class TestPasswordValidation(TestCase):
             self.fail("ValidationError was raised for valid password")
 
     @requests_mock.mock()
-    @override_settings()
     def test_custom_fail_safe_ignores_exceptions(self, m):
         validator = PWNEDPasswordValidator()
         password = "doesntmatter"
@@ -141,3 +140,12 @@ class TestPasswordValidation(TestCase):
             self.assertEqual(e.message, "failure")
             return
         self.fail("No exception was thrown")
+
+    def test_help_text(self):
+        validator = PWNEDPasswordValidator()
+        self.assertEqual(validator.get_help_text(), validator.help_text)
+
+    @override_settings(PWNED_VALIDATOR_HELP_TEXT="help!")
+    def test_custom_help_text(self):
+        validator = PWNEDPasswordValidator()
+        self.assertEqual(validator.get_help_text(), "help!")
